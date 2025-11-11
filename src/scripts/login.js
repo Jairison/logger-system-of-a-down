aa6129dcc28a49de8fcb369ee9aa133aabd6f484
jairison <jairisonsousa93@gmail.com>
2025-11-11 12:11:22 -0300

    ultimo commit

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -1,25 +1,42 @@
 
 const username = document.querySelector('#username-login')
 const password = document.querySelector('#password-login')
-const checkbox = document.querySelector('#checkbox-login')
+const checkbox = document.querySelector('#checkbox')
 const button  = document.querySelector('#button-login')
 
 button.addEventListener('click', (event) => {
+    event.preventDefault();
 
    const usernameValue = username.value;
    const passwordValue = password.value;
 
    if (usernameValue === '' || passwordValue === '') {
-    event.preventDefault();
+
     alert('Please fill in all fields!');
     return;
    }
 
-   if (usernameValue !== 'Jairison' || passwordValue !== '1010') {
-    event.preventDefault();
-    alert('Incorrect username or password');
-    return;
+   const usernameSession = sessionStorage.getItem('username')
+   const passwordSession = sessionStorage.getItem('password')
+
+   if (usernameValue !== usernameSession || passwordValue !== passwordSession){
+      alert('Incorrect username or password!')
+      return;
    }
 
    alert('Access granted');
+
+   window.location.href = './home.html';
+
+});
+
+checkbox.addEventListener('change', () => {
+ if(checkbox.checked) {
+   password.type ='text'
+ } else{
+   password.type ='password'
+ }
+
+   
+
 })
